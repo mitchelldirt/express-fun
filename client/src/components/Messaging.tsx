ed3ce737f22101e782606e3969810d31b4dc5304
mitchelldirt <mitchellmudd@outlook.com>
2023-05-12 14:22:42 -0400

    Client?

---
+++ client/src/components/Messaging.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+export default function Messaging() {
+  const [ws, setWs] = useState<WebSocket>();
+
+  const [receivedMessages, setMessages] = useState<string[]>([]);
+  const [sentMessages, setSentMessages] = useState<string[]>([]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const message = new FormData(event.currentTarget).get("message");
+
+    if (ws && message && typeof message === "string") {
+      ws.send(message);
+      setSentMessages((messages) => [...messages, message]);
+    }
+  };
+
+  useEffect(() => {
+    // Create a new WebSocket instance
+    const newWs = new WebSocket("ws://localhost:8081");
+
+    newWs.addEventListener("open", () => {
+      console.log("WebSocket connection established.");
+      setWs(newWs);
+    });
+
+    newWs.addEventListener("message", (event) => {
+      setMessages((messages) => [...messages, event.data]);
+    });
+
+    newWs.addEventListener("close", () => {
+      console.log("WebSocket connection closed. Reconnecting...");
+      // Restart the WebSocket connection after a delay
+      setTimeout(() => {
+        console.log("Reconnecting...");
+        setWs(newWs);
+
+        if (newWs.readyState !== 0 && newWs.readyState !== 1) {
+          newWs.close();
+          alert("WebSocket connection failed.");
+          return;
+        }
+      }, 2000);
+    });
+  }, [setWs]);
+
+  return (
+    <>
+      <h1>Messaging</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="message">Message</label>
+        <input name="message" type="text" id="message" />
+        <button type="submit">Send</button>
+      </form>
+
+      <h2>Received Messages</h2>
+      <ul>
+        {receivedMessages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+
+      <h2>Sent Messages</h2>
+      <ul>
+        {sentMessages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
